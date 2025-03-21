d2805dcdbcee05e16d918e64fc63a85b2e44cf4a
Edilane <edilanedasilvaalves@gmail.com>
2025-03-21 12:02:19 -0400

    feat: Ajuste da paginaIniciar com o simulador

---
+++ PagIniciar/PaginaIniciar.js
@@ -0,0 +1,11 @@
+document.getElementById("startButton").addEventListener("click", function () {
+  document.body.style.backgroundColor = "#2ecc71";
+  setTimeout(function () {
+    window.location.href = "../AppSimulador/SimuladorEmbarque.html";
+  }, 500);
+});
+
+setTimeout(() => {
+  document.querySelector(".pallet").style.animation =
+    "loadPallet 2s infinite alternate ease-in-out";
+}, 4000);
