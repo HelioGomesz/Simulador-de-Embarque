9a4b819abfcbcf2615c41e4ee890759801b392f4
Edilane <edilanedasilvaalves@gmail.com>
2025-03-21 08:29:36 -0400

    Style: Tela Login

--- PagLogin/ValidarLogin.js
+++ PagLogin/ValidarLogin.js
@@ -9,15 +9,14 @@ let senhaLogin = document.getElementById("password");
 const validarUsuario = "admin@lumicenter.com";
 const validarSenha = "1234";
 
-
 // Evento de click no botão login
-botaoLogin.addEventListener("click", function() {
-    const pegarUsuario = emailLogin.value; // obter valor do campo usuário do HTML
-    const pegarSenha = senhaLogin.value; // obter valor do campo senha do HTML
-    if (pegarUsuario === validarUsuario && pegarSenha === validarSenha){       //Verificar se os dados inseridos correspondem às credenciais padrão
-        window.location.href = "../AppSimulador/Simulador.html";    //Função de redirecionar para página desejada
-    } else {
-        alert("Usuário ou Senha incorretos"); //Alertar quando houver erro de entrada
-    }
-
+botaoLogin.addEventListener("click", function () {
+  const pegarUsuario = emailLogin.value; // obter valor do campo usuário do HTML
+  const pegarSenha = senhaLogin.value; // obter valor do campo senha do HTML
+  if (pegarUsuario === validarUsuario && pegarSenha === validarSenha) {
+    //Verificar se os dados inseridos correspondem às credenciais padrão
+    window.location.href = "../AppSimulador/SimuladorEmbarque.html"; //Função de redirecionar para página desejada
+  } else {
+    alert("Usuário ou Senha incorretos"); //Alertar quando houver erro de entrada
+  }
 });
