2bee715afdc305d7a35c0a8cb66be4e6b801d601
Edilane <edilanedasilvaalves@gmail.com>
2025-03-27 17:44:20 -0400

    Feat: Mudança de link da pagina iniciar para o simulador

--- PagIniciar/PaginaIniciar.js
+++ PagIniciar/PaginaIniciar.js
@@ -1,7 +1,7 @@
 document.getElementById("startButton").addEventListener("click", function () {
   document.body.style.backgroundColor = "#2ecc71";
   setTimeout(function () {
-    window.location.href = "../Teste_Tela_Simulador/teste5.html";
+    window.location.href = "../Teste_Tela_Simulador/simuladorRev5.html";
   }, 500);
 });
 
