a24d3a57d0c64165b4b9be71d4cafd247cce8ea5
Edilane <edilanedasilvaalves@gmail.com>
2025-06-06 11:46:29 -0400

    Style:  Alteração da animação da pagina e link com simulador Revisão 7.0

--- PagIniciar/PaginaIniciar.js
+++ PagIniciar/PaginaIniciar.js
@@ -1,11 +1,10 @@
 document.getElementById("startButton").addEventListener("click", function () {
   document.body.style.backgroundColor = "#2ecc71";
   setTimeout(function () {
-    window.location.href = "../simuladorRev6.html";
+    window.location.href = "../simuladorDeEmbarque_7.0.html";
   }, 500);
 });
 
 setTimeout(() => {
-  document.querySelector(".pallet").style.animation =
-    "loadPallet 2s infinite alternate ease-in-out";
+  document.querySelector(".pallet").style.animation = "loadPallet 10s";
 }, 4000);
