eb058d372d00b49f31701408f914bf18ce2aa6e8
Hélio <heliosgomes02@gmail.com>
2025-06-16 13:10:40 -0400

    feat: ajuste nos links e página de configuração

--- PagIniciar/PaginaIniciar.js
+++ PagIniciar/PaginaIniciar.js
@@ -1,7 +1,7 @@
 document.getElementById("startButton").addEventListener("click", function () {
   document.body.style.backgroundColor = "#2ecc71";
   setTimeout(function () {
-    window.location.href = "../simuladorDeEmbarque_7.0.html";
+    window.location.href = "../simuladorDeEmbarque_7.0.1.html";
   }, 500);
 });
 
