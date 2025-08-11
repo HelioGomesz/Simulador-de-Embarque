bd40b50f9884693e67ac11aa31f0dd20421ee2fd
Edilane <edilanedasilvaalves@gmail.com>
2025-08-11 10:51:48 -0400

    Revert ":ok_hand: feat - Calculo de volume de unificação e correção da cubagem LM0001-8000"
    This reverts commit 2e72c84fa6270b9eda371dab0a2466904c3e2111.

--- TelaSimulador/unificacao.js
+++ TelaSimulador/unificacao.js
@@ -145,6 +145,7 @@ function unificarPalletsFisicamente(produto, quantidade, peso) {
     document.getElementById("ocupacao-container").innerText =
       ocupacao.toFixed(2) + "%";
 
+<<<<<<< HEAD
     // Volume total da unificação: quantidade total dividido pelo maior padrão CX (PP ou PG)
     const padraoCxPP = dadosProduto.PP.padraoCx || 1;
     const padraoCxPG = dadosProduto.PG.padraoCx || 1;
@@ -154,6 +155,17 @@ function unificarPalletsFisicamente(produto, quantidade, peso) {
     document.getElementById("volumeTotal-container").innerText =
       totalVolume.toFixed(2);
   });
+=======
+  alert(
+    `Pallets unificados fisicamente!\n\nPallet ${palletPequeno.getAttribute(
+      "id"
+    )} + ${palletGrande.getAttribute(
+      "id"
+    )}\nQuantidade PP: ${quantidadePP} | Quantidade PG: ${quantidadePG}\nQuantidade Total: ${quantidadeUnificada}\nPeso Total: ${pesoUnificado.toFixed(
+      2
+    )}kg`
+  );
+>>>>>>> parent of 2e72c84 (:ok_hand: feat - Calculo de volume de unificação e correção da cubagem LM0001-8000)
 }
 
 // ===Função para unificar visualmente os cubos===
@@ -423,18 +435,9 @@ function adicionarProdutoEspecialUnificado(produto, quantidade, peso) {
   document.getElementById("ocupacao-container").innerText =
     ocupacao.toFixed(2) + "%";
 
-  // Volume total da unificação: quantidade total dividido pelo maior padrão CX (PP ou PG)
-  const padraoCxPP = dadosProduto.PP.padraoCx || 1;
-  const padraoCxPG = dadosProduto.PG.padraoCx || 1;
-  const padraoCxMaior = Math.max(padraoCxPP, padraoCxPG);
-  const volumeUnificado = Math.ceil(quantidadeUnificada / padraoCxMaior);
-  totalVolume += volumeUnificado;
-  document.getElementById("volumeTotal-container").innerText =
-    totalVolume.toFixed(2);
-
   alert(
     `Produto especial ${produto} adicionado com visual unificado!\n\nQuantidade PP: ${quantidadePP} | Quantidade PG: ${quantidadePG}\nQuantidade Total: ${quantidadeUnificada}\nPeso Total: ${pesoUnificado.toFixed(
       2
-    )}kg\nVolume Total: ${volumeUnificado}`
+    )}kg`
   );
 }
