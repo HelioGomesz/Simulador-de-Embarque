3e7f68e6208ef2d4dffb09c01103b25930146e93
Helio <heliosgomes02@gmail.com>
2025-09-09 15:42:37 -0400

    bug fix: resolvendo erros do console no electron

--- TelaIniciar/PaginaIniciar.js
+++ TelaIniciar/PaginaIniciar.js
@@ -1,10 +1,18 @@
-document.getElementById("startButton").addEventListener("click", function () {
-  document.body.style.backgroundColor = "#2ecc71";
-  setTimeout(function () {
-    window.location.href = "../TelaSimulador/simuladorDeEmbarque_7.0.2.html";
-  }, 500);
-});
+document.addEventListener("DOMContentLoaded", function () {
+  const startButton = document.getElementById("startButton");
+  if (startButton) {
+    startButton.addEventListener("click", function () {
+      document.body.style.backgroundColor = "#2ecc71";
+      setTimeout(function () {
+        window.location.href = "../TelaSimulador/simuladorDeEmbarque_7.0.2.html";
+      }, 500);
+    });
+  }
 
-setTimeout(() => {
-  document.querySelector(".pallet").style.animation = "loadPallet 10s";
-}, 4000);
+  setTimeout(() => {
+    const pallet = document.querySelector(".pallet");
+    if (pallet) {
+      pallet.style.animation = "loadPallet 10s";
+    }
+  }, 4000);
+});
